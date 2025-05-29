b313f00847898e62fdcc13b098c67a211a8cd5d6
nhatlmhe160091 <nhatlmhe160091@fpt.edu.vn>
2025-05-29 22:17:43 +0700

    image table serice

--- back-end/src/services/image.service.js
+++ back-end/src/services/image.service.js
@@ -4,6 +4,27 @@ class ImageService {
     getAllImages = async () => {
         return await Image.find();
     }
+
+    createNewImage = async (data) => {
+
+        return await Image.create(data);
+    }
+
+
+    getImagesByEntityId = async (entityId) => {
+        return await Image.find({ entityId });
+    }
+
+
+    getImageById = async (imageId) => {
+        return await Image.findById(imageId);
+    }
+
+
+    deleteImageById = async (imageId) => {
+        return await Image.findByIdAndDelete(imageId);
+    }
+
 }
 
 module.exports = new ImageService;
---
+++ back-end/src/services/table.service.js
@@ -0,0 +1,7 @@
+const { Table } = require('../models/index');
+
+class TableService {
+   
+
+}
+module.exports = new TableService;
