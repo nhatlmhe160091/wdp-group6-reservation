168d8fb3a413c586a8a95d33d2173573ae2fc414
nhatlmhe160091 <nhatlmhe160091@fpt.edu.vn>
2025-05-29 22:21:44 +0700

    Update booking.service.js

--- back-end/src/services/booking.service.js
+++ back-end/src/services/booking.service.js
@@ -20,6 +20,47 @@ class BookingService {
    /**
      * author: XXX
      */
+getBookingById = async (bookingId) => {
+        const booking = await Booking.findById(bookingId).select('bookingTime adultsCount childrenCount note status reservation');
+        if (!booking) {
+            return { error: true, message: 'Booking not found' };
+        }
+        return { error: false, data: booking };
+    };
+       /**
+     * author: XXX
+     */
+    updateBookingFields = async (bookingId, bookingData) => {
+        try {
+            const allowedUpdates = {
+                bookingTime: bookingData.bookingTime,
+                adultsCount: bookingData.adultsCount,
+                childrenCount: bookingData.childrenCount,
+                note: bookingData.note,
+                status: bookingData.status
+            };
+            // if (allowedUpdates.bookingTime) {
+            //     const bookingTime = new Date(allowedUpdates.bookingTime);
+            //     if (bookingTime <= Date.now()) {
+            //         throw new Error('Thời gian đặt bàn phải ở tương lai.');
+            //     }
+            // }
+            const updatedBooking = await Booking.findByIdAndUpdate(
+                bookingId,
+                { $set: allowedUpdates },
+                { new: true, runValidators: true, context: 'query' }
+            ).select('bookingTime adultsCount childrenCount note status');
+
+            if (!updatedBooking) {
+                return { error: true, message: 'Booking not found' };
+            }
+
+            return { error: false, message: 'Booking updated successfully', data: updatedBooking };
+        } catch (error) {
+            console.error('Error updating booking:', error);
+            return { error: true, message: error.message || 'Internal Server Error' };
+        }
+    };
     
 }
 
