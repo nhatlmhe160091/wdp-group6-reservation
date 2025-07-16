f1805c49351be3ca9a7b080d4a8d58a07de3506d
Thai12341 <thaikvhe160086@fpt.edu.vn>
2025-07-16 11:52:09 +0700

    validate

--- front-end/src/utils/handleFormat.js
+++ front-end/src/utils/handleFormat.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export const formatDate = (date) => {
     const d = new Date(date);
     const day = String(d.getDate()).padStart(2, '0');
@@ -11,4 +12,19 @@ export const formatTime = (date) => {
     const hours = String(d.getHours()).padStart(2, '0');
     const minutes = String(d.getMinutes()).padStart(2, '0');
     return `${hours}h${minutes}`;
+=======
+export const formatDate = (date) => {
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    return `${day} / ${month} / ${year}`;
+};
+
+export const formatTime = (date) => {
+    const d = new Date(date);
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minutes = String(d.getMinutes()).padStart(2, '0');
+    return `${hours}h${minutes}`;
+>>>>>>> Stashed changes
 };
--- front-end/src/utils/handleScroll.js
+++ front-end/src/utils/handleScroll.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export const scrollToPosition = (targetPosition, duration) => {
     const startPosition = window.scrollY;
     const distance = targetPosition - startPosition;
@@ -16,4 +17,24 @@ export const scrollToPosition = (targetPosition, duration) => {
     };
 
     requestAnimationFrame(animation);
+=======
+export const scrollToPosition = (targetPosition, duration) => {
+    const startPosition = window.scrollY;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    const animation = currentTime => {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const progress = Math.min(timeElapsed / duration, 1);
+
+        window.scrollTo(0, startPosition + distance * progress);
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    };
+
+    requestAnimationFrame(animation);
+>>>>>>> Stashed changes
 };
